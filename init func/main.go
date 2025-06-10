b9d58da8318abdda58b31b25a562aa42b3cd195a
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-06-11 02:26:58 +0600

    anonymous func & IIFE

--- init func/main.go
+++ init func/main.go
@@ -6,9 +6,15 @@ var a = 10
 
 func main() {
 	fmt.Println("a = ", a)
+	// Anonymous function IIFE
+	func(a int, b int) {
+		c := a + b
+		fmt.Println("c = ", c)
+	}(10, 20)
 }
 
 func init() {
+	// it will execute first and before main
 	fmt.Println("first print from init func", a)
 	a = 50
 
