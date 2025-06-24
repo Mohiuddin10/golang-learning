c06d601cdcf95c56e643488bc3c05f7816a61ef0
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-06-25 02:38:06 +0600

    high order func

---
+++ high order func/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func processOperation(a int, b int, opp func(x int, y int)) {
+	opp(a, b)
+}
+
+func add(p int, q int) {
+	x := p + q
+	fmt.Println("Addition: ", x)
+}
+
+func main() {
+	fmt.Println("Starting the program")
+
+	// Passing function as an argument
+	processOperation(10, 20, add)
+
+	// Anonymous function
+	processOperation(30, 40, func(x int, y int) {
+		fmt.Println("Anonymous Addition: ", x-y)
+	})
+
+	fmt.Println("Ending the program")
+}
