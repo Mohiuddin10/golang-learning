3f2ccb050e4df41686ac591071d43fb8359a8d73
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-07-21 01:05:33 +0600

    test of internal memory

--- test/main.go
+++ test/main.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 
+var b = 10
+
+func call() {
+	// b = 30
+	fmt.Println(b)
+}
+
 func main() {
+	b = 20
+	call()
 	defer fmt.Println("x")
 	defer fmt.Println("y")
 	fmt.Println("Hello, World!")
