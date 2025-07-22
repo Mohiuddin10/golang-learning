14a8a8489a206f28d6b58c85c38ab104a1d58ae6
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-07-23 00:43:44 +0600

    clouser

---
+++ clouser/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+const a = 100
+
+var p = 10
+
+func outer() func() {
+	money := 100
+	age := 30
+	fmt.Println("Age:", age)
+	show := func() {
+		money = money + a + p
+		fmt.Println("Money:", money)
+	}
+	return show
+}
+
+func call() {
+	incr1 := outer()
+	incr1()
+	incr1()
+
+	incr2 := outer()
+	incr2()
+	incr2()
+}
+
+func init() {
+	fmt.Println("====Bank======")
+}
+
+func main() {
+	call()
+}
+
+
+
