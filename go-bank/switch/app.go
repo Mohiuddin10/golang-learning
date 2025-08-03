2b0936d506d537016af6ce00fb6edcee0be42d1f
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-08-04 01:55:34 +0600

    working on package

--- go-bank/switch/app.go
+++ go-bank/switch/app.go
@@ -35,13 +35,7 @@ func main() {
 	fmt.Println("====Welcome to Go Bank====")
 
 	for {
-
-		fmt.Println("Please enter your service code to continue...")
-		fmt.Println("1. Check Balance")
-		fmt.Println("2. Deposit Money")
-		fmt.Println("3. Withdraw Money")
-		fmt.Println("4. Exit")
-
+		presentMenu()
 		var choice int
 		fmt.Print("Enter your choice: ")
 		fmt.Scan(&choice)
---
+++ go-bank/switch/communication.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func presentMenu() {
+	fmt.Println("Please enter your service code to continue...")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw Money")
+	fmt.Println("4. Exit")
+}
