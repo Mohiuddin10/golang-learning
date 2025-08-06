4560d9b872e4f3b7d4ab06d78eb6aab614e719ec
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-08-07 02:53:19 +0600

    working on pointer max

--- test/main.go
+++ test/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Pallinder/go-randomdata"
+)
 
 var b = 10
 
@@ -9,7 +13,7 @@ func call() {
 	fmt.Println(b)
 }
 
-func add (a, b float64) float64 {
+func add(a, b float64) float64 {
 	return a + b
 }
 
@@ -19,5 +23,8 @@ func main() {
 	defer fmt.Println("x")
 	defer fmt.Println("y")
 	fmt.Println("Hello, World!")
-	num1 :=add(15,20)
+	num1 := add(15, 20)
+	fmt.Println("Sum:", num1)
+
+	fmt.Println("Random Name:", randomdata.FullName(randomdata.Female))
 }
