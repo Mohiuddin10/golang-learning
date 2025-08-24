50e8ff9404ff0b35e1c6715d6dff8dab246ec991
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-08-24 19:39:01 +0600

    pass any type data in func

--- interface/main.go
+++ interface/main.go
@@ -17,8 +17,28 @@ func (p phone) turnOff() {
 	fmt.Println("Phone is off")
 }
 
+func test(data interface{}) {
+	switch data.(type) {
+	case int:
+		fmt.Println("Integer:", data)
+	case float64:
+		fmt.Println("Float64:", data)
+	case string:
+		fmt.Println("String:", data)
+	case bool:
+		fmt.Println("Boolean:", data)
+	default:
+		fmt.Println("Unknown type")
+	}
+}
+
 func main() {
 	var m Device = phone{}
 	m.turnOn()
 	m.turnOff()
+
+	test(42)
+	test("Hello")
+	test(true)
+	test(3.14)
 }
