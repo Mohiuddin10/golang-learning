e92e71fe33f4c7204485f09e6f0b09befc9e4648
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-08-26 00:31:27 +0600

    working with array task

--- generics/main.go
+++ generics/main.go
@@ -6,6 +6,17 @@ func main() {
 	result := add(1.12, 2.32)
 	result = result + 10.0
 	fmt.Println("result:", result)
+
+	var numbers = [6]int{1, 2, 3, 4, 5, 6}
+	num1 := numbers[1:3]
+	fmt.Println("Numbers slice:", num1)
+	fmt.Println("Numbers length:", len(num1))
+	fmt.Println("Numbers capacity:", cap(num1))
+
+	num1 = num1[:4]
+	fmt.Println("Numbers slice:", num1)
+	fmt.Println("Numbers length:", len(num1))
+	fmt.Println("Numbers capacity:", cap(num1))
 }
 
 func add[T int | float64 | string](a, b T) T {
---
+++ task-array/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hobbies = [3]string{"Reading", "Traveling", "Gaming"}
+	fmt.Println("Hobbies:", hobbies)
+}
