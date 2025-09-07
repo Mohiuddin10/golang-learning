6150a9b24f6ca1b2883ded1b5eaa41da778b11d3
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-09-07 11:01:43 +0600

    working on for loop

---
+++ function/main.go
@@ -0,0 +1,69 @@
+// use function as value
+// anonymous function
+// recursion function
+// variadic function
+
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	double := doubleNumbers(&numbers)
+	fmt.Println(double)
+	fmt.Println(numbers)
+
+	addTen := plusTen(&numbers)
+	fmt.Println(addTen)
+	fmt.Println(numbers)
+
+	// get func as value
+	multyplay := transformNumber(&numbers, multiplyBy)
+	fmt.Println("multy", multyplay)
+
+	// take a number that multyplay by the element of {}
+
+	multyplayBy := multiplayOp(&numbers, multiPlayOperation, 5)
+	fmt.Println("multyplayBy", multyplayBy)
+
+}
+
+func doubleNumbers(numbers *[]int) []int {
+	dNumbers := []int{}
+	for _, value := range *numbers {
+		dNumbers = append(dNumbers, value*2)
+	}
+	return dNumbers
+}
+
+func plusTen(numbers *[]int) []int {
+	plusTenNumbers := []int{}
+	for _, value := range *numbers {
+		plusTenNumbers = append(plusTenNumbers, value+10)
+	}
+	return plusTenNumbers
+}
+
+func transformNumber(numbers *[]int, f func(int) int) []int {
+	newNumbers := []int{}
+	for _, v := range *numbers {
+		newNumbers = append(newNumbers, f(v))
+	}
+	return newNumbers
+}
+
+func multiplyBy(n int) int {
+	return n * 3
+}
+
+func multiplayOp(numbers *[]int, f func(int, int) int, mNum int) []int {
+	newNumbers := []int{}
+	for _, v := range *numbers {
+		newNumbers = append(newNumbers, f(v, mNum))
+	}
+	return newNumbers
+}
+
+func multiPlayOperation(n, m int) int {
+	return n * m
+}
