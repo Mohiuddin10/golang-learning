509a1abe6440477cee25f2b2146a583f048a5bf2
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-09-11 11:32:36 +0600

    func return func

--- test/main.go
+++ test/main.go
@@ -3,6 +3,23 @@ package main
 import "fmt"
 
 type Operation func(int, int) int
+type plusTwo func(int) int
+
+// a func that return a func that adds 2 to all numbers of the array
+
+func addTwo() plusTwo {
+	return func(n int) int {
+		return n + 2
+	}
+}
+
+func applyToAll(nums *[]int, plusTwo plusTwo) {
+	plusNumbers := []int{}
+	for _, value := range *nums {
+		plusNumbers = append(plusNumbers, plusTwo(value))
+	}
+	fmt.Println(plusNumbers)
+}
 
 func add(a, b int) int {
 	return a + b
@@ -16,6 +33,8 @@ func multiplay(a, b int) int {
 	return a * b
 }
 func main() {
+	numbers := []int{10, 2, 3, 4}
+	applyToAll(&numbers, addTwo())
 
 	f := Operation(add)
 
