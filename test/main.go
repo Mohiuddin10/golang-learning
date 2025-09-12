9e9299a1976fe3155a84ae5ce72200fe8281527d
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-09-12 10:38:11 +0600

    ann func

--- test/main.go
+++ test/main.go
@@ -2,48 +2,18 @@ package main
 
 import "fmt"
 
-type Operation func(int, int) int
-type plusTwo func(int) int
-
-// a func that return a func that adds 2 to all numbers of the array
-
-func addTwo() plusTwo {
-	return func(n int) int {
-		return n + 2
+func makeAdder(x int) func(int) int {
+	return func(y int) int {
+		return x + y
 	}
 }
 
-func applyToAll(nums *[]int, plusTwo plusTwo) {
-	plusNumbers := []int{}
-	for _, value := range *nums {
-		plusNumbers = append(plusNumbers, plusTwo(value))
-	}
-	fmt.Println(plusNumbers)
-}
-
-func add(a, b int) int {
-	return a + b
-}
-
-func minus(a, b int) int {
-	return a - b
-}
-
-func multiplay(a, b int) int {
-	return a * b
-}
 func main() {
-	numbers := []int{10, 2, 3, 4}
-	applyToAll(&numbers, addTwo())
 
-	f := Operation(add)
+	add10 := makeAdder(20)
+	fmt.Println(add10(10))
 
-	fmt.Println("10 + 5 =", f(10, 5))
-
-	g := Operation(minus)
-
-	fmt.Println("10 - 5 =", g(10, 5))
-	h := Operation(multiplay)
-	fmt.Println("10 * 5 =", h(10, 5))
+	add20 := makeAdder(30)
+	fmt.Println(add20(20))
 
 }
