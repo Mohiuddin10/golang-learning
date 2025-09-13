bafa49e9648490ab62e3d1a41c80b67652f8882c
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-09-13 12:21:35 +0600

    ann fun call

--- test/main.go
+++ test/main.go
@@ -8,8 +8,26 @@ func makeAdder(x int) func(int) int {
 	}
 }
 
+func transformNumber(x *[]int, f func(int) int) int{
+	if (*x)[0] > 10 {
+		doubleNumber := []int{}
+		for _, v := range *x {
+			doubleNumber = append(doubleNumber, f(v))
+		}
+		return doubleNumber
+	}
+	return nil
+}
+
+func double(x int) int {
+	return x * 2
+}
+
 func main() {
 
+	numbers := []int{11, 14, 17}
+	dNumbers := transformNumber(&numbers, double)
+	fmt.Println(dNumbers)
 	add10 := makeAdder(20)
 	fmt.Println(add10(10))
 
