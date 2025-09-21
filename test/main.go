ef09ac959a5dbe38ad1cc5d83a7a7aa95ab25ec1
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-09-22 01:56:25 +0600

    clouser

--- test/main.go
+++ test/main.go
@@ -2,22 +2,28 @@ package main
 
 import "fmt"
 
-func devideByTwo(a int) int {
-	const b = 2
-	return a / b
+func main() {
+	numbers := []int{1, 2, 3, 4, 5}
+	double := createTransformar(2)
+	triple := createTransformar(3)
+
+	doubled := transformNumber(&numbers, double)
+	tripled := transformNumber(&numbers, triple)
+	fmt.Println("Doubled:", doubled)
+	fmt.Println("Tripled:", tripled)
+
 }
 
-func main() {
-
-	sum := func(a, b int) int {
-		return a + b
+func transformNumber(numbers *[]int, transform func(int) int) []int {
+	dNumbers := []int{}
+	for _, number := range *numbers {
+		dNumbers = append(dNumbers, transform(number))
 	}
-	fmt.Println(sum(10, 20))
-	minus := func(a, b int) int {
-		return a - b
-	}(30, 15)
-	fmt.Println(minus)
+	return dNumbers
+}
 
-	div := devideByTwo(300)
-	fmt.Println(div)
+func createTransformar(factor int) func(int) int {
+	return func(i int) int {
+		return i * factor
+	}
 }
